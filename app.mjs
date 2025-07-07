29da58fdb6deb8c9065adbe1fb779229c93cf95d
Prefrontal Corporate <prefrontalcorporate@gmail.com>
2025-07-07 19:05:42 +0000

    COMMIT

--- app.mjs
+++ app.mjs
@@ -222,6 +222,6 @@ app.use((req, res) => {
 
 // Start
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server is running on http://0.0.0.0:${port}`);
 });
